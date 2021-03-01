ee455caff6dba8cbcd2fc32a8cba70f5de283818
Karl Mcgeough <32734121+karlmcgeough@users.noreply.github.com>
2021-03-01 09:58:54 +0000

    Initial Commit

---
+++ FloatingTabBarExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloatingTabBarExample
+//
+//  Created by Karl McGeough on 01/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
